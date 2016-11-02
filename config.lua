5c72ccdb3d2c021d572127a79b866f9b362b5a56
qq1122334455q <manimoghadasini@gmail.com>
2016-11-02 14:46:12 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "235796629:AAEKsTvofbf3IyYfXfrp4PCcJtHuXEfF5uk"
+bot_token = "270640638:AAFWrTrIdCyTFYp8AlhrspOEQNBWL_HQLl8"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 180910633
+sudo_id = 87691174
